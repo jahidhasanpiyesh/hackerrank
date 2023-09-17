a132c0e0a27216211ff735658abd975ab25af391
SS COMPUTER <jahidjesan508@gmail.com>
2023-09-17 21:16:18 +0600

    simple loop using problem

---
+++ 01-Loops.py
@@ -0,0 +1,15 @@
+"""conditions :
+input = 5
+
+output =
+0
+1
+4
+9
+16
+
+"""
+# program using to loop and index for input number
+num = int(input())  # input = 5 >>>> [0,1,2,3,4] so just sqr root
+for i in range(num):
+    print(i * i)  # input * input
