29a3e4387dccf5120a05daf2e3493170cf338266
SS COMPUTER <jahidjesan508@gmail.com>
2023-09-17 21:26:18 +0600

    update

--- 01-Loops.py
+++ 01-Loops.py
@@ -1,13 +1,5 @@
-"""conditions :
-input = 5
-
-output =
-0
-1
-4
-9
-16
-
+"""
+Problem link ------:   https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
 """
 # program using to loop and index for input number
 num = int(input())  # input = 5 >>>> [0,1,2,3,4] so just sqr root
