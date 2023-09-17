6f7bfd63c28a48ad818a96ca7e2223df22c3f663
SS COMPUTER <jahidjesan508@gmail.com>
2023-09-17 21:26:46 +0600

    new problem solve

---
+++ 02-Print Function.py
@@ -0,0 +1,9 @@
+"""
+problem link ----:    https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+"""
+# input data indexing ways data access. use to conditions while loop !
+n = int(input())
+a = 1  # fix start point
+while a <= n:  # use to condition while
+    print(a, end="")  # just try end=""call to output 1234...n type same
+    a = a + 1  # index +1 increment
