0e25b6af04e042e4a57fb6e8220087c2a8fe49c5
SS COMPUTER <jahidjesan508@gmail.com>
2023-09-17 22:06:32 +0600

    next problem solve

---
+++ 03-List Comprehensions.py
@@ -0,0 +1,18 @@
+
+# remove the comments for input function >>>>>>>
+x = int(input("Enter the first input:"))
+y = int(input("Enter the second input:"))
+z = int(input("Enter the thread input:"))
+n = int(input("Enter the condition input != data:"))
+
+# data = 5
+# for i in range(data+1):
+#     print(i)   # 0 1 2 3 4 5  index wys data insert
+# conditions >>>  [i, j, k] wys data in comprehensions
+
+result = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if (i+j+k) != n]
+print("Output is hear :", result)
+
+
+
+
